a8943cfe03118c31cc088bc7b2358d677d35e654
Raphael D. Pinheiro <raphaklaus@gmail.com>
2017-01-03 22:23:41 -0200

    feat: add moving platforms

--- boxes/game.js
+++ boxes/game.js
@@ -1,34 +1,60 @@
-var game = new Phaser.Game(1024, 680, Phaser.AUTO, 'Boxes', {preload: preload,
-    create: create, update: update});
+var game = new Phaser.Game(1024, 480, Phaser.AUTO, 'Boxes', {preload: preload,
+    create: create, update: update, render: render});
 
 var box;
-var speed = 5;
+var largeBox;
+var jumping = false;
+var speed = 300;
 
 function preload() {
   game.load.image('box', 'assets/box.png');
+  game.load.image('largebox', 'assets/largebox.png');
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.physics.arcade.gravity.y = 800;
   box = game.add.sprite(50, game.world.centerY, 'box');
-  var boxer = game.add.sprite(game.world.centerX, game.world.centerY, 'box');
+  largeBox = game.add.sprite(0, game.world.height - 30, 'largebox');
 
-  game.physics.arcade.enable([box, boxer]);
+  game.physics.arcade.enable([box, largeBox]);
   box.body.gravity.y = 200;
-  boxer.body.gravity.y = 1000;
+  largeBox.body.allowGravity = false;
+  largeBox.body.immovable = true;
 
   box.body.collideWorldBounds = true;
-  boxer.body.collideWorldBounds = true;
-
-  box.body.bounce.y = 0.6;
-  boxer.body.bounce.y = 0.7;
 }
 
 function update() {
-  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    box.x -= speed;
-  } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-    box.x += speed;
+  game.physics.arcade.collide(box, largeBox);
+
+  if (box.body.touching.down && jumping) {
+    console.log('hmmmm')
+    jumping = false;
   }
+
+  if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && !jumping) {
+    jumping = true;
+    box.body.velocity.y = -400;
+    console.log('key pressed');
+  }
+
+  if (jumping)
+    box.body.velocity.x = 0;
+  else 
+    box.body.velocity.x = 200;
+
+  largeBox.body.velocity.x = -200;
+
+  // if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+  //   box.body.velocity.x = -speed;
+  // }
+
+  // if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+  //   box.body.velocity.x = speed;
+  // }
+}
+
+function render() {
+  game.debug.text(box.body.x, 10, 10, '#FFFFFF');
 }
